15af35db17a005a01e3534319ed59bf24ad5f4a0
d0hyeon <ehgus6887@gmail.com>
2021-07-03 00:44:05 +0900

    feat: sort promise 버전 타입 추가

--- src/algorithm/sort/type.ts
+++ src/algorithm/sort/type.ts
@@ -1,3 +1,4 @@
 
 export type SortFunction<T = number> = (array: T[]) => T[];
 export type Sort<T = number> = (array: T[], sortFunction: SortFunction<T>) => T[]; 
+export type SortPromise<T = number> = (array: T[], sortFunction: SortFunction<T>) => Promise<T[]>;
